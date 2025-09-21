d827c3fb802137c562dfde1b86dddca41b5cc85b
Robotz213 <nicholas@robotz.de>
2025-09-21 19:16:49 -0300

    ✨ *refactor: melhorar a estrutura e estilo do componente SobreView*
    Refatoração do componente SobreView para simplificar a estrutura do template e melhorar a legibilidade do código. As alterações incluem a remoção de um div desnecessário e ajustes nos estilos para uma apresentação mais consistente.
    
    Files Changed:
    - *src/views/home/components/SobreView.vue*: removido div extra, ajustados estilos de padding, tamanho da fonte e largura do parágrafo.

--- src/views/home/components/SobreView.vue
+++ src/views/home/components/SobreView.vue
@@ -1,39 +1,36 @@
 <template>
-  <div class="content">
-    <div class="text-sobre">
-      <h1 class="header-sobre">Sobre a Comunidade</h1>
-      <p class="paragrafo-sobre">
-        <span style="font-weight: bold">DEVstão</span> é a organização oficial da comunidade de
-        desenvolvedores do <span style="font-weight: bold">Renato Impera</span> (vulgo PARDO). Um
-        espaço criado para estudo, networking e desenvolvimento de projetos colaborativos.
-      </p>
-    </div>
+  <div class="text-sobre">
+    <h1 class="header-sobre">Sobre a Comunidade</h1>
+    <p class="paragrafo-sobre">
+      <span style="font-weight: bold">DEVstão</span> é a organização oficial da comunidade de
+      desenvolvedores do <span style="font-weight: bold">Renato Impera</span> (vulgo PARDO). Um
+      espaço criado para estudo, networking e desenvolvimento de projetos colaborativos.
+    </p>
   </div>
 </template>
 
 <style lang="css" scoped>
-.content {
+.text-sobre {
   margin: 50px;
   margin-top: 150px;
-}
-
-.text-sobre {
   border-radius: 10px;
   border-style: solid;
   border-width: 3px;
-  padding: 15px;
+  padding: 25px;
+  height: 100%;
+  width: 50%;
 }
 
 .text-sobre .header-sobre {
   font-weight: bold;
-  font-size: 2rem;
+  font-size: 3.2rem;
   font-family: 'Times New Roman', Times, serif;
 }
 
 .text-sobre .paragrafo-sobre {
-  width: 50%;
-
-  font-size: 1.1rem;
+  margin-top: 90px;
+  width: 75%;
+  font-size: 1.3rem;
   font-family:
     'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana,
     sans-serif;
