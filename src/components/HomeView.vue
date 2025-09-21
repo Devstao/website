468f3b7e9a6895a0cab32e13fb525d19640c4e7c
Robotz213 <nicholas@robotz.de>
2025-09-21 16:59:35 -0300

    Refactor project structure by removing unused components, styles, and assets; update routing to use HomeView component.

---
+++ src/components/HomeView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="on-center">
+    <h1>Ainda sem nada por Aqui</h1>
+
+    <p class="paragraph">
+      Enquanto isso, entre na
+      <a class="link-bavarian-grove" href="https://linktr.ee/BavarianGrove">nossa comunidade</a>
+    </p>
+  </div>
+</template>
+
+<style lang="css">
+.on-center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.paragraph {
+  text-align: center;
+  margin-top: 5rem;
+
+  font-weight: bold;
+}
+
+.link-bavarian-grove {
+  color: #3b82f6;
+  text-decoration: none;
+}
+
+.link-bavarian-grove:hover {
+  animation: rainbow 1.5s linear infinite;
+  background: linear-gradient(90deg, red, orange, yellow, green, blue, indigo, violet);
+  background-size: 400% 100%;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+@keyframes rainbow {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 100% 50%;
+  }
+}
+</style>
