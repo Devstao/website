2c55585889d5ae73c1a7d13a5d68639a65f9bd3e
Robotz213 <nicholas@robotz.de>
2025-09-21 23:13:51 -0300

    ✨ *feat: adicionar redirecionamento automático para a página Sobre*
    Implementa um redirecionamento automático para a página 'sobre' após 8 segundos na visualização inicial.
    
    Files Changed:
    - *src/views/home/HomeView.vue*: adiciona lógica de redirecionamento usando onMounted e useRouter.

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import MainView from './components/MainView.vue'
+
+const router = useRouter()
+onMounted(() => {
+  setTimeout(() => {
+    router.push({ name: 'sobre' })
+  }, 8000) // Redireciona após 8 segundos (8000 milissegundos)
+})
 </script>
 
 <template>
